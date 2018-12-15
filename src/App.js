1cb4e0da9466a3603333d9bbff6f850ad41bdb10
Julia <yfedyakova@tugush.com>
2018-12-15 17:38:15 +0300

    first lesson

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+import styled from 'styled-components';
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="wrapper">
+                <SayFullName name="Julia" surname="Fedyakova" link="https://vk.com/yuliya_kf" />
+                <SayFullName name="Jul" surname="Fed" link="https://vk.com/yuliya_kf" />
+                <SayFullName name="J" surname="F" link="https://vk.com/yuliya_kf" />
+            </div>
+        );
+    }
 }
+
+
+function SayFullName(props) {
+    return (
+        <div>
+            <h1>My name is {props.name}, my surname is {props.surname}</h1>
+            <a href={props.link}>Link to my profile</a>
+        </div>
+    )
+}
+
+
 
 export default App;
