a024eada1a4ca38e3b18e3053e692ed733216ed3
Julia <yfedyakova@tugush.com>
2018-12-16 13:32:34 +0300

    second lesson

--- src/App.js
+++ src/App.js
@@ -8,29 +8,54 @@ import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + 'img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 50px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+    height: 90px;
+    padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`
 
 class App extends Component {
     render() {
         return (
-            <div className="wrapper">
-                <SayFullName name="Julia" surname="Fedyakova" link="https://vk.com/yuliya_kf" />
-                <SayFullName name="Jul" surname="Fed" link="https://vk.com/yuliya_kf" />
-                <SayFullName name="J" surname="F" link="https://vk.com/yuliya_kf" />
+            <div className="App">
+                 <HeaderWrapper>
+                     <Grid>
+                         <Header/>
+                     </Grid>
+                 </HeaderWrapper>
+                <MenuWrapper>
+                    <Grid>
+                        <Menu/>
+                    </Grid>
+                </MenuWrapper>
+                <MainWrapper>
+                    <Grid>
+                        <Main/>
+                    </Grid>
+                </MainWrapper>
             </div>
         );
     }
 }
-
-
-function SayFullName(props) {
-    return (
-        <div>
-            <h1>My name is {props.name}, my surname is {props.surname}</h1>
-            <a href={props.link}>Link to my profile</a>
-        </div>
-    )
-}
-
 
 
 export default App;
---
+++ src/components/Link.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 300;
+    padding-right: 33px;
+    &:not(:last-child) {
+        border-right: 1px dotted #fff;
+    }
+    &:hover {
+        color: #8d8d8d;
+        text-decoration: none;
+    }
+`
+
+class Link extends React.Component {
+    render() {
+        return (
+            <StyledLink href={this.props.link}>{this.props.text}</StyledLink>
+        )
+    }
+}
+
+export default Link;
